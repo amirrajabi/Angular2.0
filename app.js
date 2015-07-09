386cefa0086334b9ed1a8c9196e657f04333abd2
amirrajabi <amir@amirrajabi.com>
2015-07-09 21:35:07 +1000

    Added new project files

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//# sourceMappingURL=app.js.map
+/**
+ * Created by amirrajabi on 8/07/15.
+ */
+if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+if (typeof __metadata !== "function") __metadata = function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var angular2_1 = require("angular2/angular2");
+var HelloWorld = (function () {
+    function HelloWorld() {
+        this.names = ['Amir', 'somy', 'Rahi', 'Arash'];
+    }
+    HelloWorld = __decorate([
+        angular2_1.Component({
+            selector: 'hello-world'
+        }),
+        angular2_1.View({
+            directives: [angular2_1.For],
+            template: "<ul><li *for=\"#name of names\">Hello {{ name }}</li></ul>"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], HelloWorld);
+    return HelloWorld;
+})();
+angular2_1.bootstrap(HelloWorld);
--- app.ts
+++ app.ts
@@ -1,3 +1,34 @@
-//<reference path="typings/angular2/angular2.d.ts" >
+/**
+ * Created by amirrajabi on 8/07/15.
+ */
 
-import {Component, View, bootstrap} from 'angular2/angular2';
+import{
+    Component,
+    View,
+    For,
+    bootstrap
+    } from "angular2/angular2"
+
+@Component({
+    selector : 'hello-world'
+})
+@View({
+
+    directives: [For],
+    //template: `<article>Hello {{ name }}</article>`
+    template: `<ul><li *for="#name of names">Hello {{ name }}</li></ul>`
+
+})
+
+class HelloWorld{
+
+    //name: string;
+    names: Array;
+    constructor(){
+        //this.name = 'Amir';
+        this.names = ['Amir', 'somy', 'Rahi', 'Arash']
+    }
+
+}
+
+bootstrap(HelloWorld);
